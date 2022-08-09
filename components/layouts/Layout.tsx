42d5654f940a67738b304ff9cde1e3ddf03ec263
adriancoll <acoll@aiudo.es>
2022-08-09 16:36:04 +0200

    feat: add og meta tags

--- components/layouts/Layout.tsx
+++ components/layouts/Layout.tsx
@@ -6,6 +6,8 @@ import { Navbar } from "../ui";
 
 type Props = { children: JSX.Element | JSX.Element[]; title: string };
 
+const origin = typeof window === "undefined" ? "" : window.location.origin;
+
 export const Layout: FC<Props> = ({ children, title }) => {
   return (
     <>
@@ -16,7 +18,12 @@ export const Layout: FC<Props> = ({ children, title }) => {
           name="description"
           content={`Información sobre el pokémon ${title}`}
         />
+
         <meta name="keywords" content={`${title}, pokemon, pokedex`} />
+
+        <meta property="og:title" content={`Información sobre ${title}`} />
+        <meta property="og:description" content={`Esta es la página sobre ${title}`} />
+        <meta property="og:image" content={`${origin}/img/banner.jpg`} />
       </Head>
 
       <Navbar />
